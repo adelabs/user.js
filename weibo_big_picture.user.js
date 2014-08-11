48eccc73eb5ec3abcd168cf9e6c3fa110815910e
adelabs <adelabs@users.noreply.github.com>
2014-08-11 14:15:46 +0800

    Update weibo_big_picture.user.js

--- weibo_big_picture.user.js
+++ weibo_big_picture.user.js
@@ -4,15 +4,12 @@
 // @description     New buttons for opening full sized pictures in new background tabs. Add "href" attributes to "Full size"/"查看大图"/"查看大圖" anchors so that you can mid-click or right-click them with more options.
 // @version         3.2.1
 // @license         GPL version 3
+// @downloadURL     https://github.com/adelabs/user.js/raw/master/weibo_big_picture.user.js
 // @include         *://weibo.com/*
 // @require         http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @grant           GM_openInTab
 // @run-at          document-end
 // ==/UserScript==
-
-/*
- *  https://gist.github.com/adelabs/7f483736baca2d5c1c90/raw/weibo_big_picture.user.js
- */
 
 function create_button(text, hrefs) {
     var a = $('<a class="W_btn_b"><span>' + text + '</span></a>');
