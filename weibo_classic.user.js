6041ed42529aece517ccecfe2951b3b93ec56d4f
adelabs <adelabs@users.noreply.github.com>
2014-11-03 11:23:32 +0800

    Update weibo_classic.user.js

--- weibo_classic.user.js
+++ weibo_classic.user.js
@@ -10,7 +10,7 @@
 // @run-at          document-start
 // ==/UserScript==
 
-//http://www.w3schools.com/js/js_cookies.asp
+// http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
